e07f4c50d68879d269ff630bf770e585095f413d
bhaumikdebanshu <bhaumikdebanshu@gmail.com>
2022-03-16 20:28:12 +0530

    code formatted and commented

--- js/efficacy-modal.js
+++ js/efficacy-modal.js
@@ -1,10 +1,11 @@
+// Fetching and adding Efficacy modal data from json
 $.getJSON(dataPath, function (data) {
     document.getElementById("efficacy-heading").innerHTML = data.efficacyHeading
     document.getElementById("efficacy-image").src = path + "/images/efficacy.png"
     document.getElementById("close-efficacy-modal").innerHTML = "Close"
 });
 
+// Emptying Efficacy modal content
 window.closeEfficacyModal = () =>{
-    // document.getElementById("review-modal").style.display = "none"
     $("#efficacy-modal").empty();
 }
--- js/review-modal.js
+++ js/review-modal.js
@@ -1,7 +1,4 @@
-
-
-console.log(reviewBtnVal)
-
+// Generating keys to access values from data.json for the Review Hotspot clicked
 var reviewHeading = "reviewHeading" + reviewBtnVal
 var reviewStars = "reviewStars" + reviewBtnVal
 var reviewName = "reviewName" + reviewBtnVal
@@ -9,37 +6,37 @@ var reviewLocation = "reviewLocation" + reviewBtnVal
 var reviewAge = "reviewAge" + reviewBtnVal
 var review = "review" + reviewBtnVal
 
+// Fetching and adding Review modal data from json
 $.getJSON(dataPath, function (data) {
-    document.getElementById("review-heading").innerHTML = data[reviewHeading]
-    document.getElementById("stars").innerHTML = getStars(parseInt(data[reviewStars]))
-    document.getElementById("review-metrics").innerHTML = data[reviewName] + " | " + data[reviewLocation] + " | " + data[reviewAge]
-    document.getElementById("review-body").innerHTML = data[review]
-    document.getElementById("close-review-modal").innerHTML = "Close"
+  document.getElementById("review-heading").innerHTML = data[reviewHeading]
+  document.getElementById("stars").innerHTML = getStars(parseInt(data[reviewStars]))
+  document.getElementById("review-metrics").innerHTML = data[reviewName] + " | " + data[reviewLocation] + " | " + data[reviewAge]
+  document.getElementById("review-body").innerHTML = data[review]
+  document.getElementById("close-review-modal").innerHTML = "Close"
 });
 
-
-window.closeReviewModal = () =>{
-    // document.getElementById("review-modal").style.display = "none"
-    $("#review-modal").empty();
+// Emptying Review modal content
+window.closeReviewModal = () => {
+  $("#review-modal").empty();
 }
 
+// Creating visual stars from floating pt values
 function getStars(rating) {
+  // Round to nearest half
+  rating = Math.round(rating * 2) / 2;
+  let output = [];
 
-    // Round to nearest half
-    rating = Math.round(rating * 2) / 2;
-    let output = [];
-  
-    // Append all the filled whole stars
-    for (var i = rating; i >= 1; i--)
-      output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-  
-    // If there is a half a star, append it
-    if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-  
-    // Fill the empty stars
-    for (let i = (5 - rating); i >= 1; i--)
-      output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
-  
-    return output.join('');
-  
-  }
+  // Append all the filled whole stars
+  for (var i = rating; i >= 1; i--)
+    output.push('<i class="fa fa-star" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  // If there is a half a star, append it
+  if (i == .5) output.push('<i class="fa fa-star-half-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  // Fill the empty stars
+  for (let i = (5 - rating); i >= 1; i--)
+    output.push('<i class="fa fa-star-o" aria-hidden="true" style="color: gold;"></i>&nbsp;');
+
+  return output.join('');
+
+}
